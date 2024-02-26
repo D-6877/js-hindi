f0069fb4bd96125291f176ea5ae01631a3aff09f
D-6877 <123552981+D-6877@users.noreply.github.com>
2024-02-26 15:09:32 +0000

    datatype

--- basic.js
+++ basic.js
@@ -1,2 +1,21 @@
-console.log("hello ");
-console.log("welcome");
+
+const accountid = 14453
+let accountemail = "hello@gmail.com"
+
+var accountpadssword = "1234";
+
+accountcity = "jaipur";
+
+
+// accountid =2;
+accountemail = "hello8786@gmail.com";
+
+accountpadssword = 2933982;
+accountcity = "bangalore";
+
+let acountstate;
+
+
+
+console.table([accountid, accountemail,accountpadssword,accountcity,accountstate]);
+
