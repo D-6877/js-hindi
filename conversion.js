08ab07fce8a744740df7b7053ddb113248532c6d
D-6877 <123552981+D-6877@users.noreply.github.com>
2024-02-26 15:53:04 +0000

    conversion

---
+++ conversion.js
@@ -0,0 +1,40 @@
+let score="debsanakr";
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueinnumber = Number(score);
+console.log(valueinnumber);
+console.log(typeof valueinnumber);
+
+// //"33" => 33
+// //"33abc"  => NaN
+// //true = > 1 ; false=>0
+
+
+
+let islogin = "Desankar";
+
+let booleanislogin = Boolean(islogin);
+
+console.log(islogin);
+
+console.log(booleanislogin);
+
+
+
+//1 --> true;  0-->flase
+
+//" " ---->false
+//"Debsankar" --> true;
+
+
+
+let somenumber = 33;
+
+let stringnumber = String(somenumber);
+
+console.log(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
--- datatype.js
+++ datatype.js
@@ -1,1 +1,22 @@
-"use strict"
+"use strict"  //treat all js code as newer version
+
+// alert(3+3);  we are use node js not browser
+
+console.log(3+3);   
+
+let name =" Debsankar";
+let age = 18;
+let islogin = true;
+
+//number  => 2 to powe 53;
+//bigint --> large value
+// string  --> doube quote;
+//boolean = true/false
+
+//null  --> standalone value
+//undefine => 
+//symbol ==> unique  
+
+console.log(typeof "deb");
+
+
