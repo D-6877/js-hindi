807bbfe8d8dfedbafa63de9a7fecc5ca38475e32
D-6877 <123552981+D-6877@users.noreply.github.com>
2024-02-27 06:57:42 +0000

    comparison

---
+++ comparison.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+//this type comparison shuld  be avoid 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);    //output-->flase;
+// console.log(null==0);         //output-->flase;
+// console.log((null >= 0));     //output--->true
+
+
+//console.log(undefined >= 0 );  //output -->flase
+//console.log(undefined == 0 );       //output-->flase;
+// console.log(undefined < 0 );           //output-->flase;
+// console.log(undefined <= 0 );           //output-->flase;
+
+
+//strictly check(===)
+
+console.log("2"===2);
+
