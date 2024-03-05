beffb7bcc47b2ee7eca19987f8ce02243b205a97
D-6877 <123552981+D-6877@users.noreply.github.com>
2024-03-05 06:14:37 +0000

    conditional statement

---
+++ control_flow.js
@@ -0,0 +1,87 @@
+// if statement
+
+//if(condition){     // condition must be true
+//}
+
+// const userlosggedin = true;
+
+// if( userlosggedin)
+// {
+//     console.log("user already log in!");
+// }
+
+
+//relational operator
+// <, >, <=,>= ,!= ,===, !==
+
+// if(2==="2")         // strick checking 
+// {
+//     console.log("it is equal");
+// }
+// else
+// {
+//     console.log("not equal");     
+// }
+
+// if(power)   // pwer undefined
+// {
+    
+//      let power = 2434;
+//     console.log(`the value of the power ${power}`);   // in this case power is undefined
+// }
+
+const balance = 1000;
+// if(balance > 500 ) console.log("test"),console.log("test2");   // we can write this type but it is very ad syntax 
+
+
+
+//if, else, else if statement
+
+// const userloggedin =  true;
+// const debitcard = false;
+
+// if(userloggedin || debitcard)     // logical operator && , ||, !
+// {
+//     console.log("you can shoping");
+// }
+// else{
+//     console.log("not");
+// }
+
+//switch case statement
+
+// switch(key){
+//     case 1:
+//         break;
+    
+
+//     default: 
+//         break;
+// }
+
+
+const month =6;
+
+switch (month) {
+    case 1:
+        {
+        console.log("it is january!");
+        break;
+       }
+        
+    case 2:
+        console.log("it is february!");
+        break;
+    case 3:
+        console.log("it is march!");
+        break;
+
+    default:
+        console.log("defalut case match!");
+        break;
+}
+
+
+
+
+
