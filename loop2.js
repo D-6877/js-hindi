20169d376887c63e5d2d89484ccc62a159f227de
D-6877 <123552981+D-6877@users.noreply.github.com>
2024-03-05 15:14:55 +0000

    while and do-while loop

---
+++ loop2.js
@@ -0,0 +1,31 @@
+// while
+
+// while (condition) {
+    
+// }
+
+// let num = 0;
+// while( num < 10)
+// {
+//     console.log(num);
+//     num++;
+// }
+
+// let myarray = [5,3,52,2,1];
+// let i =0;
+// while(i<myarray.length)
+// {
+//     console.log(myarray[i]);
+//     i++;
+// }
+
+
+
+// do while loop
+let i=0;
+do{
+    console.log(i);
+    i++;
+}
+while(i<10)
+
