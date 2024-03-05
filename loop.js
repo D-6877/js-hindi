307f178d62d2d578e539a324d4b26af38408442c
D-6877 <123552981+D-6877@users.noreply.github.com>
2024-03-05 14:52:58 +0000

    just for loop

---
+++ loop.js
@@ -0,0 +1,57 @@
+// for iteration 
+
+// for (let index = 0; index < 10; index++) {
+    
+//     if( index==5)
+//     {
+//         console.log("5 is best number!");
+//     }
+//     console.log(index);
+// }
+
+
+// for(let i =0;i<10;i++)
+// {
+//     console.log(`outer loop ${i}`);
+//     for(let j=0;j < 10;j++)
+//     {
+//         //console.log(`inner loop ${j} `);
+//         console.log( i+ " * " + j+" = "+(i*j));
+//     }
+// }
+
+
+
+// const arr = ["somu","canu","jamu","basu"];
+
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]+ " ");
+// }
+
+
+// keyword in loop
+//break and continue
+
+for(let i=0;i<10;i++)
+{
+    
+    if(i==5){
+       continue;
+    }
+    console.log(i);
+
+}
+
+
+// copy shortcut ------->1. select 2. alt + shift + down arrow
+
+for(let i=0;i<10;i++)
+{
+    
+    if(i==5){
+       continue;
+    }
+    console.log(i);
+
+}
