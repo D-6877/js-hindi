cb1ba9a88c06057fcafa6f0c8a98cf26850af355
D-6877 <123552981+D-6877@users.noreply.github.com>
2024-03-09 05:40:14 +0000

    DOM manipuletion

--- DOM.js
+++ DOM.js
@@ -51,7 +51,8 @@ document.querySelector("h1");  // --> it is use  access  the first query which i
 document.querySelector("h2");  // ---> it is the same 
 
 
-// access the id and class with quer selecter method
+// access the id and class with quer selecter method ---> in query selector return only one value
+
 document.querySelector("#title");  // access the id  which is write with the # symbol
 document.querySelector(".heading");  // access the class which is write with . symbol
 
@@ -64,3 +65,50 @@ const turngreen = myunorderlist.querySelector("li");  // to store the value of t
 turngreen.style.backgroundColor = "green";   //  perform the style operation in which the background color will be green
 turngreen.innerText;  // perform muliple operation
 turngreen.innerText = "four";  // change the value of the first list.
+
+
+
+// querySelectorAll----> method which is holds all value in the form of nodelist
+
+document.querySelectorAll("li");  //  ---->  we can check prototype 
+
+//it is one type of array but it is not array it is similar to array 
+const templilist =  document.querySelectorAll("li");
+// but it has not any map prototype
+
+// if i want to change the color of listlike that
+// templilist.style.color = green;   // ---> it does not work because it is in the form of array  that's whay i heave to use like that
+
+templilist[0].style.color = green;
+templilist[0].style.textDecoration = "underline";
+
+// use forEch loop        ---------------->  shift+enter
+templilist.forEach(function (li){
+    li.style.color = "green";
+    })
+
+
+
+// another example
+const myh1 = document.querySelectorAll("h1");
+myh1[0].style.color = "green";
+
+
+// html collection   which is different the node list
+document.getElementsByClassName("list");//----> it return the html collection on which we can not perform any loop because it has not any loop prototype
+const list = document.getElementsByClassName("list");
+
+//it have to convert into the array
+const convertarr = Array.from(list);// it is array method
+convertarr.forEach(function (li){    // use for each loop
+    li.style.color= "green";
+})
+
+
+//another example
+headline.forEach(function (h) {
+    h.style.color = "black";
+    h.style.backgroundColor = "red";
+    h.style.padding = "30px";
+    h.textContent = "debsankar";
+})
